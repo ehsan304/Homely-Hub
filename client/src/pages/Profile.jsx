15fa2010f5ffb555b3077c3a33d010ae7f6a8ec7
ehsan304 <muhammadehsan304@gmail.com>
2024-06-10 14:11:01 +0500

    upload image functionality

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,16 +1,75 @@
-import { useDebugValue } from 'react'
-import {useSelector} from 'react-redux'
+import { useEffect, useRef, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage'
+import { app } from '../firebase.js'
 
 export default function Profile() {
-    const {currentUser} = useSelector(state => state.user)
+    const fileRef = useRef(null)
+    const { currentUser } = useSelector(state => state.user)
+    const [file, setFile] = useState(undefined)
+    const [filePerc, setFilePerc] = useState(0)
+    const [fileUploadError, setFileUploadError] = useState(false)
+    const [formData, setFormData] = useState({})
+    console.log(fileUploadError)
+    console.log(filePerc)
+    // console.log(file)
+    console.log(formData)
+
+    useEffect(() => {
+        if (file) {
+            handleFileUpload(file)
+        }
+    }, [file])
+
+    const handleFileUpload = (file) => {
+        const storage = getStorage(app)
+        const fileName = new Date().getTime() + file.name
+        const storageRef = ref(storage, fileName)
+        const uploadTask = uploadBytesResumable(storageRef, file)
+
+        uploadTask.on('state_changed',
+            (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                setFilePerc(Math.round(progress))
+            },
+
+            (error) => {
+                setFileUploadError(true)
+            },
+
+            () => {
+                getDownloadURL(uploadTask.snapshot.ref).then
+                    ((downloadURL) =>
+                        setFormData({ ...formData, avatar: downloadURL })
+                    )
+            }
+
+        );
+
+    }
+
     return (
-        <div className='p-3 max-w-lg mx-auto'> 
+        <div className='p-3 max-w-lg mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-5'>Profile</h1>
             <form className='flex flex-col gap-4'>
-                <img src={currentUser.avatar} alt="Profile Pic"  className='rounded-full self-center h-24 w-24 object-cover cursor-pointer mt-2' />
-                <input type='text' placeholder='username' className='rounded-lg border p-3' id='username'  />
-                <input type='email' placeholder='email' className='rounded-lg border p-3' id='email'  />
-                <input type='password' placeholder='password' className='rounded-lg border p-3' id='password'  />
+                <input onChange={(e) => setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*' />
+                <img onClick={() => fileRef.current.click()} src={formData.avatar || currentUser.avatar} alt="Profile Pic" className='rounded-full self-center h-24 w-24 object-cover cursor-pointer mt-2' />
+                <p className='text-sm self-center'>
+                    {fileUploadError ? (
+                        <span className='text-red-700'>
+                            Error Image upload (image must be less than 2 mb)
+                        </span>
+                    ) : filePerc > 0 && filePerc < 100 ? (
+                        <span className='text-slate-700'>{`Uploading ${filePerc}%`}</span>
+                    ) : filePerc === 100 ? (
+                        <span className='text-green-700'>Image successfully uploaded!</span>
+                    ) : (
+                        ''
+                    )}
+                </p>
+                <input type='text' placeholder='username' className='rounded-lg border p-3' id='username' />
+                <input type='email' placeholder='email' className='rounded-lg border p-3' id='email' />
+                <input type='password' placeholder='password' className='rounded-lg border p-3' id='password' />
                 <button className='bg-slate-700 p-3 rounded-lg text-white uppercase hover:opacity-90 disabled:opacity-80'>update</button>
             </form>
             <div className='flex justify-between mt-5'>
@@ -19,4 +78,5 @@ export default function Profile() {
             </div>
         </div>
     )
+
 }
