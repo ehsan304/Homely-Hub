7b8d71be07f77210c5bffe79e605816073096da7
ehsan304 <muhammadehsan304@gmail.com>
2024-06-12 20:31:09 +0500

    completed create listing ui

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -4,7 +4,7 @@ function CreateListing() {
     return (
         <main className='p-3 max-w-4xl mx-auto'>
             <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
-            <form className='flex flex-col sm:flex-row'>
+            <form className='flex flex-col sm:flex-row gap-4'>
                 <div className='flex flex-col gap-4 flex-1'>
                     <input type='text' placeholder='Name' id='name' className='border p-3 rounded-lg' />
                     <textarea type='text' placeholder='Description' id='description' className='border p-3 rounded-lg' />
@@ -31,7 +31,7 @@ function CreateListing() {
                             <span>Offer</span>
                         </div>
                     </div>
-                    <div>
+                    <div className='flex flex-wrap gap-6'>
                         <div className='flex gap-2 items-center'>
                             <input type="number" id='bedrooms' min={1}  max={10} required className='p-3 border border-gray-300 rounded-lg'/>
                             <p>Beds</p>
@@ -42,7 +42,7 @@ function CreateListing() {
                         </div>
                         <div className='flex gap-2 items-center'>
                             <input type="number" id='bedrooms' min={1}  max={10} required className='p-3 border border-gray-300 rounded-lg'/>
-                            <div>
+                            <div className='flex flex-col items-center'>
                             <p>Regular Price</p>
                             <span className='text-xs'>($ / month)</span>
                             </div>
@@ -55,6 +55,16 @@ function CreateListing() {
                             </div>
                         </div>
                     </div>
+                </div>
+                <div className='flex flex-col flex-1 gap-4'>
+                    <p className='font-semibold'>Images:
+                    <span className='font-normal text-gray-600 ml-2'>The first image will be cover (max 6)</span>
+                    </p>
+                    <div className='flex gap-4'>
+                        <input className='p-3 border border-gray-300 rounded w-full' type="file" id='images' accept='images/*' multiple />
+                        <button className='p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opacity-80'>Upload</button>
+                    </div>
+                <button className='p-3 bg-slate-700 text-white uppercase rounded-lg hover:opacity-90 disabled:opacity80'>create listing</button>
                 </div>
             </form>
         </main>
